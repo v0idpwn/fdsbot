259d18bc810ca044459cd3310779b869d2ee0c00
v0idpwn <v0idpwn@gmail.com>
2017-11-14 19:22:13 -0200

    fixed so it can run independently

--- bot.py
+++ bot.py
@@ -5,15 +5,19 @@ from os import listdir
 from botUtils import *
 from videoUpload import *
 
+while True:
+    while datetime.now().weekday() != 4:
+        print "Apparently, it is still not SEXTA FEIRA, SUA LINDA. Gotta sleep a little more..."
+        sleep(28800)
 
-while datetime.today().weekday() != 4:
-    print "Apparently, it is still not SEXTA FEIRA, SUA LINDA. Gotta sleep a little more..."
-    sleep(43200)
-
-print "FINALLY ITS sexta feira!!!!!"
-videoTweet = VideoTweet(gifToClip("gif/" + choice(listdir("gif/"))))
-videoTweet.upload_init()
-videoTweet.upload_append()
-videoTweet.upload_finalize()
-videoTweet.tweet()
-sleep(43200)
+    print "FINALLY ITS sexta feira!!!!!"
+    while  datetime.today().hour != 18: 
+        print "Still not HORA DO FINAL DO EXPEDIENTE...")
+        sleep(1200)
+    videoTweet = VideoTweet(gifToClip("gif/" + choice(listdir("gif/"))))
+    videoTweet.upload_init()
+    videoTweet.upload_append()
+    videoTweet.upload_finalize()
+    videoTweet.tweet()
+    print "Posted. Time to sleep again..."
+    sleep(86400)
