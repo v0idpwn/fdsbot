6ce1cfd569e95b1b7e71995c660522c80b7fdfb7
v0idpwn <v0idpwn@gmail.com>
2017-11-18 02:45:10 -0200

    Fixed problems with audio when uploaded to twitter

--- botUtils.py
+++ botUtils.py
@@ -6,8 +6,5 @@ def gifToClip(gifPath):
     gifClip = mpv.fx.all.loop(gifClip, duration=60)
     gifClip = gifClip.set_audio(mp.AudioFileClip("sextaFeira.mp3").subclip(14, 74))
     videoPath = gifPath.replace("gif", "mp4")
-    gifClip.write_videofile(videoPath)
+    gifClip.write_videofile(videoPath,audio_codec="aac" ,audio_bitrate="96k")
     return videoPath    
-
-
-
