8f6ae4ec7d7129ae3206522a4024da07502e91be
v0idpwn <v0idpwn@gmail.com>
2017-11-30 09:43:16 -0200

    PEP 8. Also improved overall code quality

--- bot.py
+++ bot.py
@@ -2,22 +2,26 @@ from datetime import datetime
 from time import sleep
 from random import choice
 from os import listdir
+from os.path import join
 from botUtils import *
 from videoUpload import *
 
 while True:
     while datetime.now().weekday() != 4:
-        print "Apparently, it is still not SEXTA FEIRA, SUA LINDA. Gotta sleep a little more..."
+        print "Apparently, it is still not SEXTA FEIRA, SUA LINDA..."
         sleep(28800)
 
     print "FINALLY ITS sexta feira!!!!!"
     while  datetime.today().hour != 18: 
         print "Still not HORA DO FINAL DO EXPEDIENTE..."
         sleep(1200)
-    videoTweet = VideoTweet(gifToClip("gif/" + choice(listdir("gif/"))))
-    videoTweet.upload_init()
-    videoTweet.upload_append()
-    videoTweet.upload_finalize()
-    videoTweet.tweet()
+    
+    gif_path = join("gif", choice(listdir("gif"))) 
+    video_tweet = VideoTweet(gif_to_clip(gif_path))
+    video_tweet.upload_init()
+    video_tweet.upload_append()
+    video_tweet.upload_finalize()
+    video_tweet.tweet()
+
     print "Posted. Time to sleep again..."
     sleep(86400)
--- botUtils.py
+++ botUtils.py
@@ -1,10 +1,14 @@
 import moviepy.editor as mp
 import moviepy.video as mpv
 
-def gifToClip(gifPath): 
-    gifClip = mp.VideoFileClip(gifPath, target_resolution=[640,640])
-    gifClip = mpv.fx.all.loop(gifClip, duration=60)
-    gifClip = gifClip.set_audio(mp.AudioFileClip("sextaFeira.mp3").subclip(14, 74))
-    videoPath = gifPath.replace("gif", "mp4")
-    gifClip.write_videofile(videoPath,audio_codec="aac" ,audio_bitrate="96k")
-    return videoPath    
+def gif_to_clip(gifPath): 
+    gif_clip = mp.VideoFileClip(gifPath, target_resolution=[640,640])
+    gif_clip = mpv.fx.all.loop(gifClip, duration=60)
+    gif_clip = gifClip.set_audio(
+                    mp.AudioFileClip("sextaFeira.mp3")
+                    .subclip(14, 74)
+                )
+    video_path = gifPath.replace("gif", "mp4")
+    gif_clip.write_videofile(video_path,audio_codec="aac", 
+                             audio_bitrate="96k")
+    return video_path
