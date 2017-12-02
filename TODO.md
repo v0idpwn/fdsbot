6fa6e644938310faf6e7effd8cd35a8c11ebc24d
v0idpwn <v0idpwn@gmail.com>
2017-12-01 22:25:16 -0200

    PEP 8, updated todo

--- TODO.md
+++ TODO.md
@@ -1,4 +1,6 @@
 # TO DO
 + Remove glitched gifs
 + Add better gifs
-
++ Implement phrases
++ Implement user-sent gifs (approval needed)
++ Implement error handling
--- botUtils.py
+++ botUtils.py
@@ -1,14 +1,13 @@
 import moviepy.editor as mp
 import moviepy.video as mpv
 
-def gif_to_clip(gifPath): 
-    gif_clip = mp.VideoFileClip(gifPath, target_resolution=[640,640])
-    gif_clip = mpv.fx.all.loop(gifClip, duration=60)
-    gif_clip = gifClip.set_audio(
+def gif_to_clip(gif_path): 
+    gif_clip = mp.VideoFileClip(gif_path, target_resolution=[640,640])
+    gif_clip = mpv.fx.all.loop(gif_clip, duration=60)
+    gif_clip = gif_clip.set_audio(
                     mp.AudioFileClip("sextaFeira.mp3")
-                    .subclip(14, 74)
-                )
-    video_path = gifPath.replace("gif", "mp4")
+                    .subclip(14, 74))
+    video_path = gif_path.replace("gif", "mp4")
     gif_clip.write_videofile(video_path,audio_codec="aac", 
                              audio_bitrate="96k")
     return video_path
